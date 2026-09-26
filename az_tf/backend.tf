bf74746cb4a9bfe6451133d51c1d393c62b0a1d6
manoj <muddamanojkumar@gmail.com>
2026-09-26 22:35:21 +0530

    fix: edit  backend tf file  block  in terraform

--- az_tf/backend.tf
+++ az_tf/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "azurerm" {
 resource_group_name = "lab"
-storage_account_name = "mystatetfstore"
+storage_account_name = "mytf"
 container_name = "tfstate"
 key = "aks.tfstate"
 }
