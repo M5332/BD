3846c34c7080d8bff590524908431902aa204c53
manoj <muddamanojkumar@gmail.com>
2026-09-26 21:14:05 +0530

    fix: adding missing resources group name in backedn file

--- az_tf/backend.tf
+++ az_tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "azurerm" {
-resource_group_name = var.resource_group_name
+resource_group_name = azurerm_resource_group.rg.name
 storage_account_name = "mystatetfstore"
 container_name = "tfstate"
 key = "aks.tfstate"
